4d3764a5e5d67a3ac1a00ade084f774aba9fac3c
Chronikle <81340064+Chr0nikle@users.noreply.github.com>
2024-11-10 19:15:25 +0100

    addes bsp pwm from kenneth

--- esw_uCOS3_cykit.cydsn/Generated_Source/PSoC5/Pin_Green_Led_1.c
+++ esw_uCOS3_cykit.cydsn/Generated_Source/PSoC5/Pin_Green_Led_1.c
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* File Name: Pin_Red_Led.c  
+* File Name: Pin_Green_Led_1.c  
 * Version 2.20
 *
 * Description:
@@ -15,15 +15,15 @@
 *******************************************************************************/
 
 #include "cytypes.h"
-#include "Pin_Red_Led.h"
+#include "Pin_Green_Led_1.h"
 
 /* APIs are not generated for P15[7:6] on PSoC 5 */
 #if !(CY_PSOC5A &&\
-	 Pin_Red_Led__PORT == 15 && ((Pin_Red_Led__MASK & 0xC0) != 0))
+	 Pin_Green_Led_1__PORT == 15 && ((Pin_Green_Led_1__MASK & 0xC0) != 0))
 
 
 /*******************************************************************************
-* Function Name: Pin_Red_Led_Write
+* Function Name: Pin_Green_Led_1_Write
 ****************************************************************************//**
 *
 * \brief Writes the value to the physical port (data output register), masking
@@ -52,17 +52,17 @@
 *  this function.
 *
 * \funcusage
-*  \snippet Pin_Red_Led_SUT.c usage_Pin_Red_Led_Write
+*  \snippet Pin_Green_Led_1_SUT.c usage_Pin_Green_Led_1_Write
 *******************************************************************************/
-void Pin_Red_Led_Write(uint8 value)
+void Pin_Green_Led_1_Write(uint8 value)
 {
-    uint8 staticBits = (Pin_Red_Led_DR & (uint8)(~Pin_Red_Led_MASK));
-    Pin_Red_Led_DR = staticBits | ((uint8)(value << Pin_Red_Led_SHIFT) & Pin_Red_Led_MASK);
+    uint8 staticBits = (Pin_Green_Led_1_DR & (uint8)(~Pin_Green_Led_1_MASK));
+    Pin_Green_Led_1_DR = staticBits | ((uint8)(value << Pin_Green_Led_1_SHIFT) & Pin_Green_Led_1_MASK);
 }
 
 
 /*******************************************************************************
-* Function Name: Pin_Red_Led_SetDriveMode
+* Function Name: Pin_Green_Led_1_SetDriveMode
 ****************************************************************************//**
 *
 * \brief Sets the drive mode for each of the Pins component's pins.
@@ -85,16 +85,16 @@ void Pin_Red_Led_Write(uint8 value)
 *  APIs (primary method) or disable interrupts around this function.
 *
 * \funcusage
-*  \snippet Pin_Red_Led_SUT.c usage_Pin_Red_Led_SetDriveMode
+*  \snippet Pin_Green_Led_1_SUT.c usage_Pin_Green_Led_1_SetDriveMode
 *******************************************************************************/
-void Pin_Red_Led_SetDriveMode(uint8 mode)
+void Pin_Green_Led_1_SetDriveMode(uint8 mode)
 {
-	CyPins_SetPinDriveMode(Pin_Red_Led_0, mode);
+	CyPins_SetPinDriveMode(Pin_Green_Led_1_0, mode);
 }
 
 
 /*******************************************************************************
-* Function Name: Pin_Red_Led_Read
+* Function Name: Pin_Green_Led_1_Read
 ****************************************************************************//**
 *
 * \brief Reads the associated physical port (pin status register) and masks 
@@ -108,16 +108,16 @@ void Pin_Red_Led_SetDriveMode(uint8 mode)
 *  The current value for the pins in the component as a right justified number.
 *
 * \funcusage
-*  \snippet Pin_Red_Led_SUT.c usage_Pin_Red_Led_Read  
+*  \snippet Pin_Green_Led_1_SUT.c usage_Pin_Green_Led_1_Read  
 *******************************************************************************/
-uint8 Pin_Red_Led_Read(void)
+uint8 Pin_Green_Led_1_Read(void)
 {
-    return (Pin_Red_Led_PS & Pin_Red_Led_MASK) >> Pin_Red_Led_SHIFT;
+    return (Pin_Green_Led_1_PS & Pin_Green_Led_1_MASK) >> Pin_Green_Led_1_SHIFT;
 }
 
 
 /*******************************************************************************
-* Function Name: Pin_Red_Led_ReadDataReg
+* Function Name: Pin_Green_Led_1_ReadDataReg
 ****************************************************************************//**
 *
 * \brief Reads the associated physical port's data output register and masks 
@@ -126,8 +126,8 @@ uint8 Pin_Red_Led_Read(void)
 *
 * The data output register controls the signal applied to the physical pin in 
 * conjunction with the drive mode parameter. This is not the same as the 
-* preferred Pin_Red_Led_Read() API because the 
-* Pin_Red_Led_ReadDataReg() reads the data register instead of the status 
+* preferred Pin_Green_Led_1_Read() API because the 
+* Pin_Green_Led_1_ReadDataReg() reads the data register instead of the status 
 * register. For output pins this is a useful function to determine the value 
 * just written to the pin.
 *
@@ -136,19 +136,19 @@ uint8 Pin_Red_Led_Read(void)
 *  justified number for the component instance.
 *
 * \funcusage
-*  \snippet Pin_Red_Led_SUT.c usage_Pin_Red_Led_ReadDataReg 
+*  \snippet Pin_Green_Led_1_SUT.c usage_Pin_Green_Led_1_ReadDataReg 
 *******************************************************************************/
-uint8 Pin_Red_Led_ReadDataReg(void)
+uint8 Pin_Green_Led_1_ReadDataReg(void)
 {
-    return (Pin_Red_Led_DR & Pin_Red_Led_MASK) >> Pin_Red_Led_SHIFT;
+    return (Pin_Green_Led_1_DR & Pin_Green_Led_1_MASK) >> Pin_Green_Led_1_SHIFT;
 }
 
 
 /* If interrupt is connected for this Pins component */ 
-#if defined(Pin_Red_Led_INTSTAT) 
+#if defined(Pin_Green_Led_1_INTSTAT) 
 
     /*******************************************************************************
-    * Function Name: Pin_Red_Led_SetInterruptMode
+    * Function Name: Pin_Green_Led_1_SetInterruptMode
     ****************************************************************************//**
     *
     * \brief Configures the interrupt mode for each of the Pins component's
@@ -161,12 +161,12 @@ uint8 Pin_Red_Led_ReadDataReg(void)
     * \param position
     *  The pin position as listed in the Pins component. You may OR these to be 
     *  able to configure the interrupt mode of multiple pins within a Pins 
-    *  component. Or you may use Pin_Red_Led_INTR_ALL to configure the
+    *  component. Or you may use Pin_Green_Led_1_INTR_ALL to configure the
     *  interrupt mode of all the pins in the Pins component.       
-    *  - Pin_Red_Led_0_INTR       (First pin in the list)
-    *  - Pin_Red_Led_1_INTR       (Second pin in the list)
+    *  - Pin_Green_Led_1_0_INTR       (First pin in the list)
+    *  - Pin_Green_Led_1_1_INTR       (Second pin in the list)
     *  - ...
-    *  - Pin_Red_Led_INTR_ALL     (All pins in Pins component)
+    *  - Pin_Green_Led_1_INTR_ALL     (All pins in Pins component)
     *
     * \param mode
     *  Interrupt mode for the selected pins. Valid options are documented in
@@ -182,19 +182,19 @@ uint8 Pin_Red_Led_ReadDataReg(void)
     *  port.
     *
     * \funcusage
-    *  \snippet Pin_Red_Led_SUT.c usage_Pin_Red_Led_SetInterruptMode
+    *  \snippet Pin_Green_Led_1_SUT.c usage_Pin_Green_Led_1_SetInterruptMode
     *******************************************************************************/
-    void Pin_Red_Led_SetInterruptMode(uint16 position, uint16 mode)
+    void Pin_Green_Led_1_SetInterruptMode(uint16 position, uint16 mode)
     {
-		if((position & Pin_Red_Led_0_INTR) != 0u) 
+		if((position & Pin_Green_Led_1_0_INTR) != 0u) 
 		{ 
-			 Pin_Red_Led_0_INTTYPE_REG = (uint8)mode; 
+			 Pin_Green_Led_1_0_INTTYPE_REG = (uint8)mode; 
 		}
     }
     
     
     /*******************************************************************************
-    * Function Name: Pin_Red_Led_ClearInterrupt
+    * Function Name: Pin_Green_Led_1_ClearInterrupt
     ****************************************************************************//**
     *
     * \brief Clears any active interrupts attached with the component and returns 
@@ -211,11 +211,11 @@ uint8 Pin_Red_Led_ReadDataReg(void)
     *  those associated with the Pins component.
     *
     * \funcusage
-    *  \snippet Pin_Red_Led_SUT.c usage_Pin_Red_Led_ClearInterrupt
+    *  \snippet Pin_Green_Led_1_SUT.c usage_Pin_Green_Led_1_ClearInterrupt
     *******************************************************************************/
-    uint8 Pin_Red_Led_ClearInterrupt(void)
+    uint8 Pin_Green_Led_1_ClearInterrupt(void)
     {
-        return (Pin_Red_Led_INTSTAT & Pin_Red_Led_MASK) >> Pin_Red_Led_SHIFT;
+        return (Pin_Green_Led_1_INTSTAT & Pin_Green_Led_1_MASK) >> Pin_Green_Led_1_SHIFT;
     }
 
 #endif /* If Interrupts Are Enabled for this Pins component */ 
--- esw_uCOS3_cykit.cydsn/codegentemp/esw_uCOS3_cykit_u.sdc
+++ esw_uCOS3_cykit.cydsn/codegentemp/esw_uCOS3_cykit_u.sdc
@@ -1,3 +1,3 @@
 # Component constraints for C:\Users\Chronikle\Documents\GitHub\BodyAnalyzer\esw_uCOS3_cykit.cydsn\TopDesign\TopDesign.cysch
 # Project: C:\Users\Chronikle\Documents\GitHub\BodyAnalyzer\esw_uCOS3_cykit.cydsn\esw_uCOS3_cykit.cyprj
-# Date: Sun, 10 Nov 2024 17:49:30 GMT
+# Date: Sun, 10 Nov 2024 18:11:09 GMT
